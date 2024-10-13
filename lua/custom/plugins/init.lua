ee0e1ef5b13371ebea5e66686f769449ff46baad
Kamila Chyla <230475+kamchy@users.noreply.github.com>
2024-10-13 09:52:01 +0200

    Change init

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,6 +6,38 @@ return {
   -- from ~/nvchad_backup/config/nvim/lua/custom
   --
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  {
+    'renerocksai/telekasten.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' }
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'gruvbox_dark',
+        -- theme = 'tokyonight',
+
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {}
+  },
+  {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
 
